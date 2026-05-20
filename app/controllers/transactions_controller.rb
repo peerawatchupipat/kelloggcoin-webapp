6ae43bb9354c9f45d0e018a057ff2845abffb58d
Peerawat.Chupipat <Peerawat.chupipat@kellogg.northwestern.edu>
2026-05-20 04:33:26 +0000

    Finished First Step in html using .find_by

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,7 @@
 class TransactionsController < ApplicationController
   def index
+    @transactions = Transaction.all
+    @users = User.all
     render :template => "transactions/index"
   end
 end
